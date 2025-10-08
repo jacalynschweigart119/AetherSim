1ed547f820d066c6b18d2372a7e38af46ae1cd15
jacalynschweigart119 <129163991+jacalynschweigart119@users.noreply.github.com>
2025-10-08 12:25:09 +0500

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,9 @@
+"""
+AetherSim Core
+---------------
+Физический движок и система агентов.
+"""
+from .physics_engine import PhysicsEngine
+from .agent_controller import Agent
+from .environment import Environment
+from .reward_system import RewardSystem
